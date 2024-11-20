adb52832deaa5aabf9cd15ed0b3974d1b1eda866
gguo78 <graceguo0708@gmail.com>
2024-11-19 19:28:55 -0500

    /fix container for APIs

--- src/api-service/docker-shell.sh
+++ src/api-service/docker-shell.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Set the image name
-export IMAGE_NAME="ocr"
+export IMAGE_NAME="api-service"
 
 # Build the Docker image
 docker build -t $IMAGE_NAME .
 
-# Run the container
-docker run --rm --name $IMAGE_NAME -p 9000:9000 $IMAGE_NAME
+# Run the container with volume mounts
+docker run --rm \
+    --name $IMAGE_NAME \
+    -p 9000:9000 \
+    -v $(pwd)/../../secrets:/app/secrets \
+    -v $(pwd)/../.env:/app/.env \
+    $IMAGE_NAME
