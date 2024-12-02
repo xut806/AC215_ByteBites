4b1c0e492634706e511a18964fe03fdd4a5695e4
Yilin Qi <yilinqi@g.harvard.edu>
2024-12-02 13:07:19 -0500

    /fix api-service entrypoint for dev vs. prod mode

--- src/api-service/docker-entrypoint.sh
+++ src/api-service/docker-entrypoint.sh
@@ -2,6 +2,25 @@
 
 echo "Starting the container..."
 
-# Run the API server
-# uvicorn api.service:app --host 0.0.0.0 --port 9000
-exec uvicorn api.service:app --host 0.0.0.0 --port 9000
+# # Run the API server
+# # uvicorn api.service:app --host 0.0.0.0 --port 9000
+# exec uvicorn api.service:app --host 0.0.0.0 --port 9000
+
+uvicorn_server() {
+    uvicorn api.service:app --host 0.0.0.0 --port 9000 --log-level debug --reload --reload-dir api/ "$@"
+}
+
+uvicorn_server_production() {
+    pipenv run uvicorn api.service:app --host 0.0.0.0 --port 9000 --lifespan on
+}
+
+export -f uvicorn_server
+export -f uvicorn_server_production
+
+if [ "${DEV}" = "1" ]; then
+  echo "Running in development mode..."
+  uvicorn_server
+else
+  echo "Running in production mode..."
+  uvicorn_server_production
+fi
