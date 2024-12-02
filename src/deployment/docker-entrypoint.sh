7d8ce4acf900b61c374642a9d134099df3d23c32
Yilin Qi <yilinqi@g.harvard.edu>
2024-12-02 15:23:34 -0500

    /fixed Ansible error for webapp
    both containers are now on the VM and web app is reachable with the VM's public IP. [NEEDS FIXING] frontend currently uses localhost, resulting in OCR not reachable

--- src/deployment/docker-entrypoint.sh
+++ src/deployment/docker-entrypoint.sh
@@ -9,14 +9,17 @@ gcloud config set project $GCP_PROJECT
 # Configure GCR
 gcloud auth configure-docker gcr.io -q
 
-args="$@"
-echo $args
+# args="$@"
+# echo $args
 
-if [[ -z ${args} ]]; 
-then
-    #/bin/bash
-    pipenv shell
-else
-  #/bin/bash $args
-  pipenv run $args
-fi
+# if [[ -z ${args} ]]; 
+# then
+#     #/bin/bash
+#     pipenv shell
+#     # pipenv run python ../api-service/api/service.py
+# else
+#   #/bin/bash $args
+#   pipenv run $args
+# fi
+
+/bin/bash
