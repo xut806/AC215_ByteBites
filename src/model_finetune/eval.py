8f362b32321eeedff81529f11be2c416231d7aff
Xu Tang <xutang@g.harvard.edu>
2024-12-06 00:37:58 -0500

    add evalution script

---
+++ src/model_finetune/eval.py
@@ -0,0 +1,31 @@
+import evaluate
+import torch
+from unsloth import FastLanguageModel
+from datasets import Dataset
+from unsloth import is_bfloat16_supported
+# from datasets import load_dataset
+
+def evaluate_model(model, tokenizer, eval_dataset, device, max_new_tokens=2500):
+    FastLanguageModel.for_inference(model)
+    model.to(device)
+
+    bleu_metric = evaluate.load('bleu')
+    predictions, references = [], []
+    
+    # Process each example in the dataset
+    for example in eval_dataset:
+        prompt = example['prompt']
+        completion = example['completion']
+
+        inputs = tokenizer(prompt, return_tensors="pt", max_length=tokenizer.model_max_length, truncation=True).to(device)
+        outputs = model.generate(**inputs, max_new_tokens=max_new_tokens, use_cache = True)
+        
+        decoded_preds = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        decoded_labels = [example['completion']]  
+        
+        predictions.extend(decoded_preds)
+        references.extend([[ref] for ref in decoded_labels])
+    
+    result = bleu_metric.compute(predictions=predictions, references=references)
+    return result['bleu']
+
