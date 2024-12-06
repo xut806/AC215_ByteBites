6229a68a4832d40158161b51db8595395abb2735
Xu Tang <xutang@g.harvard.edu>
2024-12-06 03:15:16 -0500

    update files for model evaluation

--- src/model_finetune/llama_finetune.py
+++ src/model_finetune/llama_finetune.py
@@ -1,7 +1,7 @@
 import argparse
 from trl import SFTConfig, SFTTrainer
 import torch
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 import pandas as pd
 from unsloth import FastLanguageModel
 from google.cloud import storage
@@ -11,6 +11,9 @@ import subprocess
 import shutil
 import wandb
 from huggingface_hub import login
+from eval import evaluate_model
+import zipfile
+import re
 
 # logging_dir = "/app/logs"
 
@@ -67,6 +70,53 @@ def upload_to_gcs(bucket_name, destination_blob_name, source_file_path):
         print(f"Failed to upload {source_file_path} "
               f"to {bucket_name}/{destination_blob_name}: {e}")
 
+def get_latest_checkpoint(destination_folder="./finetuned_model"):
+    try:
+        checkpoints = [d for d in os.listdir(destination_folder) if os.path.isdir(os.path.join(destination_folder, d))]
+        sorted_checkpoints = sorted(checkpoints, key=lambda x: int(re.search(r'\d+', x).group()))
+        if not sorted_checkpoints:
+            raise Exception("No checkpoints found in the extracted model directory.")
+            
+        base_folder = destination_folder[2:] if destination_folder.startswith("./") else destination_folder
+        return f"{base_folder}/{sorted_checkpoints[-1]}"
+    except Exception as e:
+        print(f"Error finding the latest checkpoint: {e}")
+
+def download_and_extract_model(bucket_name="ai-recipe-trainer", blob_name = "finetuned_models/finetuned_model.zip", local_file_path = "finetuned_model.zip", destination_folder="./finetuned_model"):
+    try:
+        client = storage.Client()
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.download_to_filename(local_file_path)
+
+        print(f"Downloaded {blob_name} from {bucket_name} to {local_file_path}")
+        os.makedirs(destination_folder, exist_ok=True)
+        
+        with zipfile.ZipFile(local_file_path, "r") as zip_ref:
+            zip_ref.extractall(destination_folder)
+            print(f"Extracted model files to {destination_folder}")
+        
+        model_id = get_latest_checkpoint(destination_folder=destination_folder)
+        print(f"Old model's checkpoint identified: {model_id}")
+        return model_id
+    
+    except Exception as e:
+        print(f"Failed to download or extract the model: {e}")
+
+def load_and_prepare_model(model_id="finetuned_model/checkpoint-500", max_seq_length=2048, load_in_4bit = True):
+    try:
+        print(f"Loading model from: {model_id}")
+        model, tokenizer = FastLanguageModel.from_pretrained(
+            model_name=model_id,
+            max_seq_length=max_seq_length,
+            load_in_4bit=load_in_4bit
+        )
+        print("Old Finetuned model and tokenizer loaded successfully.")
+        return model, tokenizer
+
+    except Exception as e:
+        print(f"Failed to load model: {e}")
+
 
 def main(args):
     if not args.train:
@@ -74,19 +124,11 @@ def main(args):
         return
 
     bucket_name = 'ai-recipe-data'
-    # project_id = 'ai-recipe-441518' # never used
     train_blob_name = 'processed/fine_tuning_llama_train_data.jsonl'
+    val_blob_name = 'processed/fine_tuning_llama_val_data.jsonl'
+    val_data_path = f"gs://{bucket_name}/{val_blob_name}"
     MODEL_NAME = 'unsloth/Llama-3.2-3B-bnb-4bit'
-    # os.environ['GOOGLE_APPLICATION_CREDENTIALS']
-    # = '/app/secrets/data-service-account.json'
-    wandb_api_key = "50a87c65b5e1dd57a23910ad46496b75cc6a0e0b"
-    huggingface_token = "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"
-    print("Hugging Face Token fetched successfully.")
-    os.environ["WANDB_API"] = wandb_api_key
-    os.environ["HF_TOKEN"] = huggingface_token
 
-    # client = storage.Client.from_service_account_json
-    # (os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
     client = storage.Client()
     bucket = client.get_bucket(bucket_name)
     blob = bucket.blob(train_blob_name)
@@ -141,15 +183,15 @@ def main(args):
         name=NAME,
     )
     # test
-    # epochs=1
-    # batch_size=1 #4
-    # max_steps=2
-    # gradient_accumulation_steps=1
+    epochs=1
+    batch_size=1 #4
+    max_steps=2
+    gradient_accumulation_steps=1
 
-    epochs = 3
-    batch_size = 4  # 4
-    max_steps = -1
-    gradient_accumulation_steps = 4
+    # epochs = 3
+    # batch_size = 4
+    # max_steps = -1
+    # gradient_accumulation_steps = 4
     output_dir = "/app/finetuned_model"
     logging_dir = "/app/logs"
 
@@ -189,42 +231,46 @@ def main(args):
     trainer.train()
     print("Training finished...")
 
-    # Displaying the results
-    # print("Files in output_dir:")
-    # print(output_files)
+    # Evaluation: Old vs New model
+    print("Evaluating start...")
+    eva_dataset = load_dataset('json', data_files={'validation': val_data_path}, split='validation')
+    new_model_bleu = evaluate_model(model, tokenizer, eval_dataset=eva_dataset, device=torch.device("cuda"))
 
-    # print("\nFiles in logging_dir:")
-    # print(logging_files)
-    print("Saving model to Wandb...")
-    artifact_name = "finetuned_model"
-    artifact = wandb.Artifact(name=artifact_name, type="model")
-    artifact.add_dir(output_dir)
-    wandb.log_artifact(artifact)
+    old_model_id = download_and_extract_model(bucket_name="ai-recipe-trainer", blob_name = "finetuned_models/finetuned_model.zip", local_file_path = "finetuned_model.zip", destination_folder="./finetuned_model")
+    old_model, old_tokenizer = load_and_prepare_model(model_id=old_model_id, max_seq_length=2048, load_in_4bit = True)
+    old_model_bleu = evaluate_model(old_model, old_tokenizer, eval_dataset=eva_dataset, device=torch.device("cuda"))
+    print(f"New model's BlUE score: {new_model_bleu}, old model's BLUE score: {old_model_bleu}")
+    print("Evaluating end...")
 
-    print("Saving model weights to GCS...")
+    if (old_model_bleu<=new_model_bleu):
+        print("Saving new model to Wandb...")
+        artifact_name = "finetuned_model"
+        artifact = wandb.Artifact(name=artifact_name, type="model")
+        artifact.add_dir(output_dir)
+        wandb.log_artifact(artifact)
 
-    # Define paths
-    bucket_name2 = 'ai-recipe-trainer'
-    model_zip_path = "/app/finetuned_model.zip"
-    destination_blob_name = "finetuned_models/finetuned_model.zip"
+        print("Saving new model weights to GCS...")
+        bucket_name2 = 'ai-recipe-trainer'
+        model_zip_path = "/app/finetuned_model.zip"
+        destination_blob_name = "finetuned_models/finetuned_model.zip"
 
-    shutil.make_archive(
-        base_name=model_zip_path.replace('.zip', ''),
-        format='zip',
-        root_dir=output_dir)
-    print(f"Model zipped at {model_zip_path}")
+        shutil.make_archive(
+            base_name=model_zip_path.replace('.zip', ''),
+            format='zip',
+            root_dir=output_dir)
+        print(f"Model zipped at {model_zip_path}")
 
-    upload_to_gcs(bucket_name2, destination_blob_name, model_zip_path)
+        upload_to_gcs(bucket_name2, destination_blob_name, model_zip_path)
 
-    # Zip the output directory
-    # subprocess.run(["zip", "-r", model_zip_path, output_dir])
+        # Zip the output directory
+        # subprocess.run(["zip", "-r", model_zip_path, output_dir])
 
-    # Upload to GCS
-    # upload_to_gcs(bucket_name2, gcs_blob_name, model_zip_path)
+        # Upload to GCS
+        # upload_to_gcs(bucket_name2, gcs_blob_name, model_zip_path)
 
-    print("Model weights successfully uploaded to GCS.")
+        print("Model weights successfully uploaded to GCS.")
 
-    wandb.finish()
+        wandb.finish()
 
 #    model.save_pretrained_merged("model", tokenizer,
 #    save_method = "merged_16bit",)
