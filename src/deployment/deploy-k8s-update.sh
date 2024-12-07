f7b4a0d8cd96a38e904470addbdd34e1507bd966
gguo78 <graceguo0708@gmail.com>
2024-12-07 18:02:09 -0500

    /deploy-app: Test deployment update

--- src/deployment/deploy-k8s-update.sh
+++ src/deployment/deploy-k8s-update.sh
@@ -8,12 +8,16 @@ ansible-playbook -vv deploy-docker-images.yml -i inventory.yml
 
 echo "=== Checking generated docker tag ==="
 cat .docker-tag
+NEW_TAG=$(cat .docker-tag)
 
 echo "=== Verifying images in GCR ==="
 gcloud container images list-tags gcr.io/ac215-project-434717/bytebites-frontend --limit=3 --sort-by=~timestamp
 
 echo "=== Running update-k8s-cluster playbook ==="
 ansible-playbook -vv update-k8s-cluster.yml -i inventory-prod.yml
+
+echo "=== Force updating deployment with new image ==="
+kubectl set image deployment/frontend frontend=gcr.io/ac215-project-434717/bytebites-frontend:${NEW_TAG} -n byte-bites-app-cluster-namespace
 
 echo "=== Verifying Kubernetes Deployment ==="
 kubectl get deployment frontend -n byte-bites-app-cluster-namespace -o jsonpath='{.spec.template.spec.containers[0].image}'
